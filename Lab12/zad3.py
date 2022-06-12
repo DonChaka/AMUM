08f9f95ede2b176637c9cba99d4c37d05fc33337
DonChaka <DonChaka98@gmail.com>
2022-06-12 23:38:17 +0200

    lab 12 skonczone

--- Lab12/zad3.py
+++ Lab12/zad3.py
@@ -3,11 +3,10 @@ from numpy import array, sin, cos, radians, c_
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-plt.rcParams["figure.figsize"] = (20, 10)
 
 A = array([[1.2, 1.4], [0.8, -1.3], [-0.9, -1.1], [-1.2, 1]])
 
-fig = plt.figure()
+fig, ax = plt.subplots(1, 1)
 
 
 def animate(i):
@@ -17,15 +16,15 @@ def animate(i):
 
     extendedMatrix = c_[A, np.ones((A.shape[0], 1))]
 
-    transformedMatrix = extendedMatrix@transformationMatrix
+    transformedMatrix = extendedMatrix@transformationMatrix.T
     title = 'Przeksztalcenie afiniczne'
 
-    plt.cla()
-    plt.fill(transformedMatrix[:, 0], transformedMatrix[:, 1])
-    plt.title(title)
-    plt.xlim([0, 20])
-    plt.ylim([0, 20])
-    # plt.gca.set_aspect('equal', adjustable='box')
+    ax.clear()
+    ax.fill(transformedMatrix[:, 0], transformedMatrix[:, 1])
+    ax.set_title(title)
+    ax.set_xlim([-2, 20])
+    ax.set_ylim([-2, 20])
+    ax.set_aspect('equal', adjustable='box')
     plt.draw()
 
 
